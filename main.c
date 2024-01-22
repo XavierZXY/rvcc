26d7e3d20a7b3166069903177aa67976c65e0ab7
XavierZXY <zxy_xavier@163.com>
2024-01-22 10:25:13 +0800

    feat: :construction: [3] 支持空格

--- main.c
+++ main.c
@@ -1,5 +1,138 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief 为终结符设置种类
+ */
+typedef enum {
+  TK_RESERVED, // 符号
+  TK_NUM,      // 整数
+  TK_EOF,      // 文件结束
+} TokenKind;
+
+/**
+ * @brief 终结符结构体
+ */
+typedef struct Token Token;
+struct Token {
+  TokenKind kind; // Token种类
+  Token *next;    // 下一个Token
+  int val;        // Token的值
+  char *loc;      // Token的字符串位置
+  int len;        // Token的长度
+};
+
+/**
+ * @brief generate a new Token
+ * @param  kind              
+ * @param  start             
+ * @param  end               
+ * @return Token* 
+ */
+static Token *newToken(TokenKind kind, char *start, char *end) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->loc = start;
+  tok->len = end - start;
+  return tok;
+}
+
+/**
+ * @brief print error message and exit
+ * @param  fmt
+ * @param  ...
+ */
+static void error(char *fmt, ...) {
+  va_list ap;
+  // va_start，初始化一个va_list，用于获取可变参数
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  va_end(ap);
+  exit(1);
+}
+
+/**
+ * @brief compare Token and str
+ * @param  tok               
+ * @param  str               
+ * @return true 
+ * @return false 
+ */
+static bool equal(Token *tok, char *str) {
+  // memcmp，比较两个内存区域的内容
+  // 比较按照字典序，LHS<RHS回负值，LHS=RHS返回0，LHS>RHS返回正值
+  return memcmp(tok->loc, str, tok->len) == 0 && str[tok->len] == '\0';
+}
+
+/**
+ * @brief skip token if it is expected symbol `-`
+ * @param  tok               
+ * @param  str               
+ * @return Token* 
+ */
+static Token *skip(Token *tok, char *str) {
+  if (!equal(tok, str)) {
+    error("expected '%s'", str);
+  }
+  return tok->next;
+}
+
+/**
+ * @brief Get the Number object
+ * @param  tok               
+ * @return int 
+ */
+static int getNumber(Token *tok) {
+  if (tok->kind != TK_NUM) {
+    error("expected a number");
+  }
+
+  return tok->val;
+}
+
+/**
+ * @brief 解析过程
+ * @param  p                 
+ * @return Token* 
+ */
+static Token *tokenize(char *p) {
+  Token head = {};
+  Token *cur = &head;
+  while (*p) {
+    if (isspace(*p)) {
+      ++p;
+      continue;
+    }
+
+    // 解析数字
+    if (isdigit(*p)) {
+      cur = cur->next = newToken(TK_NUM, p, p);
+      const char *old_p = p;
+      cur->val = strtoul(p, &p, 10);
+      cur->len = p - old_p;
+      continue;
+    }
+
+    // 解析符号
+    if (*p == '+' || *p == '-') {
+      cur = cur->next = newToken(TK_RESERVED, p, p + 1);
+      ++p;
+      continue;
+    }
+
+    error("invalid token： %c", *p);
+  }
+
+  // 解析结束
+  cur->next = newToken(TK_EOF, p, p);
+
+  return head.next;
+}
 
 int main(int Argc, char **Argv) {
   // 判断传入程序的参数是否为2个，Argv[0]为程序名称，Argv[1]为传入的第一个参数
@@ -8,52 +141,37 @@ int main(int Argc, char **Argv) {
     // fprintf，格式化文件输出，往文件内写入字符串
     // stderr，异常文件（Linux一切皆文件），用于往屏幕显示异常信息
     // %s，字符串
-    fprintf(stderr, "%s: invalid number of arguments\n", Argv[0]);
-    // 程序返回值不为0时，表示存在错误
-    return 1;
+    error("%s: invalid number of arguments", Argv[0]);
   }
 
-  // P 保存输入的算式的str
-  char *P = Argv[1];
+  // 解析参数
+  Token *tok = tokenize(Argv[1]);
 
   // 声明一个全局main段，同时也是程序入口段
   printf("  .globl main\n");
   // main段标签
   printf("main:\n");
 
-  // li为addi别名指令，加载一个立即数到寄存器中
-
-  // 传入程序的参数为str类型，因为需要转换为需要long类型
-  // strtol为“string to long”，
-  // 参数为：被转换的str，str除去数字后的剩余部分，进制
-  // 传入&P，即char**, 是为了修改P的值
-
   // 这里我们将算式分解为 num (op num) (op num)...的形式
-  // 所以先将第一个num传入a0
-  printf("  li a0, %ld\n", strtol(P, &P, 10));
+  printf("  li a0, %d\n", getNumber(tok));
 
   // 解析 (op num)
-  // *P在这里判断P是否为NULL
-  while (*P) {
-    // 解析op
-    if (*P == '+') {
-      ++ P; 
-      printf("  addi a0, a0, %ld\n", strtol(P, &P, 10));
+  tok = tok->next;
+  while (tok->kind != TK_EOF) {
+    if (equal(tok, "+")) {
+      tok = tok->next;
+      printf("  addi a0, a0, %d\n", getNumber(tok));
+      tok = tok->next;
       continue;
     }
 
-    if (*P == '-') {
-      ++ P;
-      // addi中imm为有符号立即数，所以减法表示为 rd = rs1 + (-imm)
-      printf("  addi a0, a0, -%ld\n", strtol(P, &P, 10));
-      continue;
-    }
-
-    fprintf(stderr, "unexpected character: '%c'\n", *P);
-    return 1;
-
+    tok = skip(tok, "-");
+    printf("  addi a0, a0, -%d\n", getNumber(tok));
+    tok = tok->next;
   }
   
+
+  // 生成程序结束指令
   printf("  ret\n");
 
   return 0;
